d2d0d900be2fa012c0781119ffbb5c0bf2d34af5
ajcr <ajcriley@gmail.com>
2018-01-12 22:18:37 +0000

    Validate window size during init

--- rolling/base.py
+++ rolling/base.py
@@ -9,7 +9,7 @@ class RollingObject(metaclass=abc.ABCMeta):
     of basic methods needed for iteration.
     """
     def __init__(self, iterable, window_size):
-        self.window_size = window_size
+        self.window_size = self._validate_window_size(window_size)
         self._iterator = iter(iterable)
 
     def __repr__(self):
@@ -31,3 +31,12 @@ class RollingObject(metaclass=abc.ABCMeta):
     def __next__(self):
         # call self.update() and return value
         pass
+
+    @staticmethod
+    def _validate_window_size(k):
+        "Check the passed value is a positive integer"
+        if not isinstance(k, int):
+            raise TypeError('window_size must be integer type, got {}'.format(type(k).__name__))
+        if k <= 0:
+            raise ValueError('window_size must be positive')
+        return k
