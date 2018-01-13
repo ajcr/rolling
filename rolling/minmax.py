8ebe4dec0dcae38136410ed709402be437a8a0ad
ajcr <ajcriley@gmail.com>
2018-01-13 11:26:38 +0000

    Reduce code duplication

--- rolling/minmax.py
+++ rolling/minmax.py
@@ -1,84 +1,80 @@
 from collections import deque, namedtuple
+from operator import ge, le
 
 from .base import RollingObject
 
-# TODO: reduce code duplication between RollingMin and RollingMax
-
 pair = namedtuple('pair', ['value', 'death'])
 
-class RollingMin(RollingObject):
-    """Compute the minimum value in the rolling window.
 
-    Uses the ascending minima algorithm described in [1]
-    to compute each value in O(1) time and O(k) space.
+class _RollingMM(RollingObject):
+    """Internal class not for instantiation.
 
-    [1] http://www.richardhartersworld.com/cri/2001/slidingmin.html
+    Contains methods common to the Min and Max
+    classes below. These classes need only
+    implement an _update() method.
     """
-    _func_name = 'Min'
-
     def __init__(self, iterable, window_size):
         super().__init__(iterable, window_size)
         self._iterator = enumerate(self._iterator)
-
         self._buffer = deque()
-
         for _ in range(window_size - 1):
             self._update()
-
-    def _update(self):
-        buffer = self._buffer
-        i, value = next(self._iterator)
-        new_pair = pair(value, i + self.window_size)
-
-        # remove everything greater to or equal to the new value
-        while buffer and buffer[-1].value >= value:
-            buffer.pop()
-
-        buffer.append(new_pair)
-
-        # remove minimal values that died on or before this iteration
-        while buffer[0].death <= i:
-            buffer.popleft()
 
     def __next__(self):
         self._update()
         return self._buffer[0].value
 
 
-class RollingMax(RollingObject):
-    """Compute the maximum value in the rolling window.
-
-    Uses the descending maxima algorithm described in [1]
-    to compute each value in O(1) time and O(k) space.
-
-    [1] http://www.richardhartersworld.com/cri/2001/slidingmin.html
-    """
-    _func_name = 'Max'
-
-    def __init__(self, iterable, window_size):
-        super().__init__(iterable, window_size)
-        self._iterator = enumerate(self._iterator)
-
-        self._buffer = deque()
-
-        for _ in range(window_size - 1):
-            self._update()
-
+def _make_update_method(op):
+    """Helper function to make the _update() method for
+    the Min and Max classes, which differ only by op"""
     def _update(self):
         buffer = self._buffer
         i, value = next(self._iterator)
         new_pair = pair(value, i + self.window_size)
 
-        # remove everything greater to or equal to the new value
-        while buffer and buffer[-1].value <= value:
+        while buffer and op(buffer[-1].value, value):
             buffer.pop()
 
         buffer.append(new_pair)
 
-        # remove minimal values that died on or before this iteration
         while buffer[0].death <= i:
             buffer.popleft()
 
-    def __next__(self):
-        self._update()
-        return self._buffer[0].value
+    return _update
+
+
+_docstring = """Iterator object that computes the {operation}
+of a rolling window over a Python iterable.
+
+Uses the {algorithm_name} algorithm described in [1]
+to compute each value in O(1) time and O(k) space.
+
+
+The cost of updating the {operation} of each new
+window is O(1). The memory used is O(k)
+where k is the size of the window.
+
+Parameters
+----------
+
+iterable : any iterable object
+window_size : integer, the size of the rolling
+    window moving over the iterable
+
+Notes
+-----
+
+[1] http://www.richardhartersworld.com/cri/2001/slidingmin.html
+"""
+
+_mindoc = _docstring.format(operation='minimum', algorithm_name='Ascending Minima')
+_maxdoc = _docstring.format(operation='maximum', algorithm_name='Descending Maxima')
+
+RollingMin = type('RollingMin', (_RollingMM,), {'_func_name': 'Min',
+                    '_update': _make_update_method(ge),
+                    '__doc__': _mindoc})
+
+RollingMax = type('RollingMax', (_RollingMM,), {'_func_name': 'Max',
+                    '_update': _make_update_method(le),
+                    '__doc__': _maxdoc})
