929d023b4962a4029213dc7de5a0ea5e50296e39
ajcr <ajcriley@gmail.com>
2018-02-03 23:10:55 +0000

    WIP - add __new__ for window_type

--- rolling/base.py
+++ rolling/base.py
@@ -4,11 +4,23 @@ import abc
 class RollingObject(metaclass=abc.ABCMeta):
     """Baseclass for rolling iterator objects.
 
-    Note: this is an abc mainly just to enforce
-    classes to consistently implement a couple
-    of basic methods needed for iteration.
+    All child classes must implement the following:
+        -
+        -
     """
-    def __init__(self, iterable, window_size):
+    def __new__(cls, iterable, *args, window_type='fixed', **kwargs):
+        if window_type == 'fixed':
+            cls.__init__ = cls._init_fixed
+            cls.__next__ = cls._next_fixed
+        elif window_type == 'variable':
+            cls.__init__ = cls._init_variable
+            cls.__next__ = cls._next_variable
+        else:
+            raise ValueError("Unknown window_type '{}'".format(window_type))
+        cls.window_type = window_type
+        return super().__new__(cls)
+
+    def __init__(self, iterable, window_size, *args, **kwargs):
         self.window_size = self._validate_window_size(window_size)
         self._iterator = iter(iterable)
 
@@ -22,14 +34,34 @@ class RollingObject(metaclass=abc.ABCMeta):
     def __iter__(self):
         return self
 
-    @abc.abstractmethod
-    def _update(self):
-        # roll window forward and update variables
+    @abc.abstractproperty
+    def current_value(self):
+        'return the current value of the window'
         pass
 
     @abc.abstractmethod
-    def __next__(self):
-        # call self.update() and return value
+    def _init_fixed(self):
+        'intialise as a fixed-size window'
+        pass
+
+    @abc.abstractmethod
+    def _init_variable(self):
+        'intialise as a variable-size window'
+        pass
+
+    @abc.abstractmethod
+    def _remove_old(self):
+        'remove the oldest value from the window'
+        pass
+
+    @abc.abstractmethod
+    def _add_new(self):
+        'take a new value from the iterator and it to the window'
+        pass
+
+    @abc.abstractmethod
+    def _update(self):
+        'simultaneously add a new value and remove the old value'
         pass
 
     @staticmethod
