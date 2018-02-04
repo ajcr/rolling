ac39252f0705c822559bc98410fe35a5ae2daa03
ajcr <ajcriley@gmail.com>
2018-02-04 17:53:04 +0000

    WIP - fix rolling apply

--- rolling/apply.py
+++ rolling/apply.py
@@ -5,22 +5,18 @@ from .base import RollingObject
 
 
 class RollingApply(RollingObject):
-    """Apply a callable to a window"""
-    def _init_fixed(self, iterable, window_size, func, **kwargs):
+    """Apply a specified function to the rolling window"""
+
+    def _init_fixed(self, iterable, window_size, func=sum, **kwargs):
         super().__init__(iterable, window_size)
         head = islice(self._iterator, window_size - 1)
         self._buffer = deque(head, maxlen=window_size)
         self._func = func
 
-    def _init_variable(self, iterable, window_size, func, **kwargs):
-        super().__init__(iterable, window_size)
+    def _init_variable(self, iterable, window_size, func=sum, **kwargs):
+        super().__init__(iterable, window_size, **kwargs)
         self._buffer = deque(maxlen=window_size)
         self._func = func
-        self._filled = False
-
-    @property
-    def _buffer_len(self):
-        return len(self._buffer)
 
     @property
     def current_value(self):
@@ -34,26 +30,3 @@ class RollingApply(RollingObject):
 
     def _update(self):
         self._buffer.append(next(self._iterator))
-
-    def _next_fixed(self):
-        self._update()
-        return self.current_value
-
-    def _next_variable(self):
-        # while the window size is not reached, add new values
-        if not self._filled and self._buffer_len < self.window_size:
-            self._add_new()
-            if self._buffer_len == self.window_size:
-                self._filled = True
-            return self.current_value
-        # once the window size is reached, update until the iterator finishes
-        try:
-            self._add_new()
-            return self.current_value
-        except StopIteration:
-            # once iterator finishes, remove the oldest values one by one
-            if self._buffer_len == 1:
-                raise
-            else:
-                self._remove_old()
-                return self.current_value
