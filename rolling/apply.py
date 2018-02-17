ba1f88961a69518333531dd838040ded64a1a6eb
ajcr <ajcriley@gmail.com>
2018-02-17 19:03:51 +0000

    Variable windows: update rolling apply

--- rolling/apply.py
+++ rolling/apply.py
@@ -1,24 +1,36 @@
-from collections import Counter, deque
+from collections import deque
 from itertools import islice
 
 from .base import RollingObject
 
 
 class RollingApply(RollingObject):
-    """Apply any callable to a rolling window.
+    """Apply a specified function to the rolling window"""
 
-
-    """
-    def __init__(self, iterable, window_size, func):
-        super().__init__(iterable, window_size)
-
+    def _init_fixed(self, iterable, window_size, func=sum, **kwargs):
+        super().__init__(iterable, window_size, **kwargs)
         head = islice(self._iterator, window_size - 1)
         self._buffer = deque(head, maxlen=window_size)
         self._func = func
 
+    def _init_variable(self, iterable, window_size, func=sum, **kwargs):
+        super().__init__(iterable, window_size, **kwargs)
+        self._buffer = deque(maxlen=window_size)
+        self._func = func
+
+    @property
+    def current_value(self):
+        return self._func(self._buffer)
+
+    def _add_new(self):
+        self._buffer.append(next(self._iterator))
+
+    def _remove_old(self):
+        self._buffer.popleft()
+
     def _update(self):
         self._buffer.append(next(self._iterator))
 
-    def __next__(self):
-        self._update()
-        return self._func(self._buffer)
+    @property
+    def _obs(self):
+        return len(self._buffer)
