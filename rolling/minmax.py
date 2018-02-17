3f410fb5d80c53b0875821af0273c1f0b72f1d80
ajcr <ajcriley@gmail.com>
2018-02-17 19:05:41 +0000

    Variable windows: update rolling minmax

--- rolling/minmax.py
+++ rolling/minmax.py
@@ -1,88 +1,177 @@
 from collections import deque, namedtuple
 import heapq
 from itertools import islice
-from operator import ge, le
-
 
 from .base import RollingObject
 
 pair = namedtuple('pair', ['value', 'death'])
 
+# todo: reduce code duplication in the classes below
 
-class _RollingMM(RollingObject):
-    """Internal class not for instantiation.
+class RollingMin(RollingObject):
+    """Iterator object that computes the minimum
+    of a rolling window over a Python iterable.
 
-    Contains methods common to the Min and Max
-    classes below. These classes need only
-    implement an _update() method.
+    Parameters
+    ----------
+
+    iterable : any iterable object
+    window_size : integer, the size of the rolling
+        window moving over the iterable
+
+    Complexity
+    ----------
+
+    Update time:  O(1)
+    Memory usage: O(k)
+
+    where k is the size of the rolling window
+
+    Notes
+    -----
+
+    This method uses the algorithms outlined in [1].
+
+    [1] http://www.richardhartersworld.com/cri/2001/slidingmin.html
     """
-    def __init__(self, iterable, window_size):
-        super().__init__(iterable, window_size)
-        self._iterator = enumerate(self._iterator)
-        self._buffer = deque()
-        for _ in range(window_size - 1):
-            self._update()
 
-    def __next__(self):
-        self._update()
+    # Note: _obs must be tracked separately, we cannot just use
+    # the size of the buffer as the algorithm may overwrite existing
+    # values with a new value, rather than appending the value
+
+    _func_name = 'Min'
+
+    def _init_fixed(self, iterable, window_size, **kwargs):
+        super().__init__(iterable, window_size, **kwargs)
+        head = islice(self._iterator, window_size - 1)
+        self._i = -1
+        self._obs = 0
+        self._buffer = deque()
+        for value in head:
+            self._i += 1
+            self._obs += 1
+            new_pair = pair(value, self._i + self.window_size)
+            while self._buffer and self._buffer[-1].value >= value:
+                self._buffer.pop()
+            self._buffer.append(new_pair)
+
+    def _init_variable(self, iterable, window_size, **kwargs):
+        super().__init__(iterable, window_size, **kwargs)
+        self._buffer = deque()
+        self._i = -1
+        self._obs = 0
+
+    def _update(self):
+        value = next(self._iterator)
+        self._i += 1
+        new_pair = pair(value, self._i + self.window_size)
+        while self._buffer and self._buffer[-1].value >= value:
+            self._buffer.pop()
+        self._buffer.append(new_pair)
+        while self._buffer[0].death <= self._i:
+            self._buffer.popleft()
+
+    def _add_new(self):
+        value = next(self._iterator)
+        self._i += 1
+        self._obs += 1
+        new_pair = pair(value, self._i + self.window_size)
+        while self._buffer and self._buffer[-1].value >= value:
+            self._buffer.pop()
+        self._buffer.append(new_pair)
+
+    def _remove_old(self):
+        self._i += 1
+        self._obs -= 1
+        while self._buffer[0].death <= self._i:
+            self._buffer.popleft()
+
+    @property
+    def current_value(self):
         return self._buffer[0].value
 
 
-def _make_update_method(op):
-    """Helper function to make the _update() method for
-    the Min and Max classes, which differ only by op"""
+class RollingMax(RollingObject):
+    """Iterator object that computes the maximum
+    of a rolling window over a Python iterable.
+
+    Parameters
+    ----------
+
+    iterable : any iterable object
+    window_size : integer, the size of the rolling
+        window moving over the iterable
+
+    Complexity
+    ----------
+
+    Update time:  O(1)
+    Memory usage: O(k)
+
+    where k is the size of the rolling window
+
+    Notes
+    -----
+
+    This method uses the algorithms outlined in [1].
+
+    [1] http://www.richardhartersworld.com/cri/2001/slidingmin.html
+    """
+
+    # Note: _obs must be tracked separately, we cannot just use
+    # the size of the buffer as the algorithm may overwrite existing
+    # values with a new value, rather than appending the value
+
+    _func_name = 'Max'
+
+    def _init_fixed(self, iterable, window_size, **kwargs):
+        super().__init__(iterable, window_size, **kwargs)
+        head = islice(self._iterator, window_size - 1)
+        self._i = -1
+        self._obs = 0
+        self._buffer = deque()
+        for value in head:
+            self._i += 1
+            self._obs += 1
+            new_pair = pair(value, self._i + self.window_size)
+            while self._buffer and self._buffer[-1].value <= value:
+                self._buffer.pop()
+            self._buffer.append(new_pair)
+
+    def _init_variable(self, iterable, window_size, **kwargs):
+        super().__init__(iterable, window_size, **kwargs)
+        self._buffer = deque()
+        self._i = -1
+        self._obs = 0
+
     def _update(self):
-        buffer = self._buffer
-        i, value = next(self._iterator)
-        new_pair = pair(value, i + self.window_size)
+        value = next(self._iterator)
+        self._i += 1
+        new_pair = pair(value, self._i + self.window_size)
+        while self._buffer and self._buffer[-1].value <= value:
+            self._buffer.pop()
+        self._buffer.append(new_pair)
+        while self._buffer[0].death <= self._i:
+            self._buffer.popleft()
 
-        while buffer and op(buffer[-1].value, value):
-            buffer.pop()
+    def _add_new(self):
+        value = next(self._iterator)
+        self._i += 1
+        self._obs += 1
+        new_pair = pair(value, self._i + self.window_size)
+        while self._buffer and self._buffer[-1].value <= value:
+            self._buffer.pop()
+        self._buffer.append(new_pair)
 
-        buffer.append(new_pair)
+    def _remove_old(self):
+        self._i += 1
+        self._obs -= 1
+        while self._buffer[0].death <= self._i:
+            self._buffer.popleft()
 
-        while buffer[0].death <= i:
-            buffer.popleft()
-
-    return _update
-
-
-_docstring = """Iterator object that computes the {operation}
-of a rolling window over a Python iterable.
-
-Parameters
-----------
-
-iterable : any iterable object
-window_size : integer, the size of the rolling
-    window moving over the iterable
-
-Complexity
-----------
-
-Update time:  O(1)
-Memory usage: O(k)
-
-where k is the size of the rolling window
-
-Notes
------
-
-This method uses the algorithms outlined in [1].
-
-[1] http://www.richardhartersworld.com/cri/2001/slidingmin.html
-"""
-
-_mindoc = _docstring.format(operation='minimum', algorithm_name='Ascending Minima')
-_maxdoc = _docstring.format(operation='maximum', algorithm_name='Descending Maxima')
-
-RollingMin = type('RollingMin', (_RollingMM,), {'_func_name': 'Min',
-                    '_update': _make_update_method(ge),
-                    '__doc__': _mindoc})
-
-RollingMax = type('RollingMax', (_RollingMM,), {'_func_name': 'Max',
-                    '_update': _make_update_method(le),
-                    '__doc__': _maxdoc})
+    @property
+    def current_value(self):
+        return self._buffer[0].value
 
 
 class RollingMin2(RollingObject):
@@ -107,7 +196,7 @@ class RollingMin2(RollingObject):
     Notes
     -----
 
-    This method uses heap to keep track of the minimum
+    This method uses a heap to keep track of the minimum
     value in the rolling window.
 
     Items that expire are lazily deleted, which can mean
@@ -116,22 +205,41 @@ class RollingMin2(RollingObject):
     """
     _func_name = 'Min2'
 
-    def __init__(self, iterable, window_size):
-        super().__init__(iterable, window_size)
-        self._iterator = enumerate(self._iterator)
-
-        self._heap = [(value, i + window_size) for i, value in islice(self._iterator, window_size - 1)]
+    def _init_fixed(self, iterable, window_size, **kwargs):
+        super().__init__(iterable, window_size, **kwargs)
+        head = islice(self._iterator, window_size - 1)
+        self._heap = [pair(value, i + window_size) for i, value in enumerate(head)]
         heapq.heapify(self._heap)
+        self._i = len(self._heap) - 1
+        self._obs = len(self._heap)
+
+    def _init_variable(self, iterable, window_size, **kwargs):
+        super().__init__(iterable, window_size, **kwargs)
+        self._heap = []
+        self._i = -1
+        self._obs = 0
 
     def _update(self):
-        i, value = next(self._iterator)
-        new_pair = (value, i + self.window_size)
-
+        value = next(self._iterator)
+        self._i += 1
+        new_pair = pair(value, self._i + self.window_size)
         heapq.heappush(self._heap, new_pair)
-
-        while self._heap[0][1] <= i:
+        while self._heap[0].death <= self._i:
             heapq.heappop(self._heap)
 
-    def __next__(self):
-        self._update()
-        return self._heap[0][0]
+    def _add_new(self):
+        value = next(self._iterator)
+        self._i += 1
+        self._obs += 1
+        new_pair = pair(value, self._i + self.window_size)
+        heapq.heappush(self._heap, new_pair)
+
+    def _remove_old(self):
+        self._i += 1
+        self._obs -= 1
+        while self._heap[0].death <= self._i:
+            heapq.heappop(self._heap)
+
+    @property
+    def current_value(self):
+        return self._heap[0].value
--- tests/test_minmax.py
+++ tests/test_minmax.py
@@ -2,6 +2,8 @@ import pytest
 
 from rolling.minmax import RollingMin, RollingMax, RollingMin2
 
+# todo: remove duplication of test data
+
 min_test_data = [
     ([3, 0, 1, 7, 2], 5, [0]),
     ([3, 0, 1, 7, 2], 4, [0, 0]),
@@ -102,4 +104,55 @@ def test_rolling_min2(array, window_size, expected):
 ])
 def test_rolling_max(array, window_size, expected):
     r = RollingMax(array, window_size)
+    assert list(r) == expected
+
+@pytest.mark.parametrize('array,window_size,expected', [
+    ([3, 0, 1, 7, 2], 5, [3, 0, 0, 0, 0, 0, 1, 2, 2]),
+    ([3, 0, 1, 7, 2], 4, [3, 0, 0, 0, 0, 1, 2, 2]),
+    ([3, 0, 1, 7, 2], 3, [3, 0, 0, 0, 1, 2, 2]),
+    ([3, 0, 1, 7, 2], 2, [3, 0, 0, 1, 2, 2]),
+    ([3, 0, 1, 7, 2], 1, [3, 0, 1, 7, 2]),
+
+    ([-8, 1, 7, -8, -9], 5, [-8, -8, -8, -8, -9, -9, -9, -9, -9]),
+    ([-8, 1, 7, -8, -9], 4, [-8, -8, -8, -8, -9, -9, -9, -9]),
+    ([-8, 1, 7, -8, -9], 3, [-8, -8, -8, -8, -9, -9, -9]),
+    ([-8, 1, 7, -8, -9], 2, [-8, -8, 1, -8, -9, -9]),
+    ([-8, 1, 7, -8, -9], 1, [-8, 1, 7, -8, -9]),
+])
+def test_rolling_min_variable(array, window_size, expected):
+    r = RollingMin(array, window_size, window_type='variable')
+    assert list(r) == expected
+
+@pytest.mark.parametrize('array,window_size,expected', [
+    ([3, 0, 1, 7, 2], 5, [3, 0, 0, 0, 0, 0, 1, 2, 2]),
+    ([3, 0, 1, 7, 2], 4, [3, 0, 0, 0, 0, 1, 2, 2]),
+    ([3, 0, 1, 7, 2], 3, [3, 0, 0, 0, 1, 2, 2]),
+    ([3, 0, 1, 7, 2], 2, [3, 0, 0, 1, 2, 2]),
+    ([3, 0, 1, 7, 2], 1, [3, 0, 1, 7, 2]),
+
+    ([-8, 1, 7, -8, -9], 5, [-8, -8, -8, -8, -9, -9, -9, -9, -9]),
+    ([-8, 1, 7, -8, -9], 4, [-8, -8, -8, -8, -9, -9, -9, -9]),
+    ([-8, 1, 7, -8, -9], 3, [-8, -8, -8, -8, -9, -9, -9]),
+    ([-8, 1, 7, -8, -9], 2, [-8, -8, 1, -8, -9, -9]),
+    ([-8, 1, 7, -8, -9], 1, [-8, 1, 7, -8, -9]),
+])
+def test_rolling_min2_variable(array, window_size, expected):
+    r = RollingMin2(array, window_size, window_type='variable')
+    assert list(r) == expected
+
+@pytest.mark.parametrize('array,window_size,expected', [
+    ([3, 0, 1, 7, 2], 5, [3, 3, 3, 7, 7, 7, 7, 7, 2]),
+    ([3, 0, 1, 7, 2], 4, [3, 3, 3, 7, 7, 7, 7, 2]),
+    ([3, 0, 1, 7, 2], 3, [3, 3, 3, 7, 7, 7, 2]),
+    ([3, 0, 1, 7, 2], 2, [3, 3, 1, 7, 7, 2]),
+    ([3, 0, 1, 7, 2], 1, [3, 0, 1, 7, 2]),
+
+    ([-8, 1, 7, -8, -9], 5, [-8, 1, 7, 7, 7, 7, 7, -8, -9]),
+    ([-8, 1, 7, -8, -9], 4, [-8, 1, 7, 7, 7, 7, -8, -9]),
+    ([-8, 1, 7, -8, -9], 3, [-8, 1, 7, 7, 7, -8, -9]),
+    ([-8, 1, 7, -8, -9], 2, [-8, 1, 7, 7, -8, -9]),
+    ([-8, 1, 7, -8, -9], 1, [-8, 1, 7, -8, -9]),
+])
+def test_rolling_max_variable(array, window_size, expected):
+    r = RollingMax(array, window_size, window_type='variable')
     assert list(r) == expected
