bfddd696a42e4898bbf7880a761961f67aae8010
ajcr <ajcriley@gmail.com>
2018-02-22 21:15:19 +0000

    Tidy logical classes

--- rolling/logical.py
+++ rolling/logical.py
@@ -47,19 +47,18 @@ class All(RollingObject):
 
     """
     def _init_fixed(self, iterable, window_size, **kwargs):
-        head = islice(self._iterator, window_size - 1)
         self._i = 0
+        self._obs = window_size
         self._last_false = -1
-        for val in head:
+        for val in islice(self._iterator, window_size - 1):
             self._i += 1
             if not val:
                 self._last_false = self._i
-        self._obs = window_size
 
     def _init_variable(self, iterable, window_size, **kwargs):
         self._i = -1
         self._obs = 0
-        self._last_false = -window_size - 1
+        self._last_false = -1
 
     def _add_new(self):
         val = next(self._iterator)
@@ -125,19 +124,18 @@ class Any(RollingObject):
 
     """
     def _init_fixed(self, iterable, window_size, **kwargs):
-        head = islice(self._iterator, window_size - 1)
         self._i = 0
+        self._obs = window_size
         self._last_true = -1
-        for val in head:
+        for val in islice(self._iterator, window_size - 1):
             self._i += 1
             if val:
                 self._last_true = self._i
-        self._obs = window_size
 
     def _init_variable(self, iterable, window_size, **kwargs):
         self._i = -1
         self._obs = 0
-        self._last_true = -window_size - 1
+        self._last_true = -1
 
     def _add_new(self):
         val = next(self._iterator)
