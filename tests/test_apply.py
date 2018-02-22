aee43e90b901ed11017fe293b5a033062f60597c
ajcr <ajcriley@gmail.com>
2018-02-22 20:27:43 +0000

    Add tests for rolling Apply

---
+++ tests/test_apply.py
@@ -0,0 +1,31 @@
+import pytest
+
+from rolling.apply import Apply
+
+@pytest.mark.parametrize('array', [[3, 6, 5, 8, 1]])
+@pytest.mark.parametrize('window_size,expected', [
+    (6, []),
+    (5, [[3, 6, 5, 8, 1]]),
+    (4, [[3, 6, 5, 8], [6, 5, 8, 1]]),
+    (3, [[3, 6, 5], [6, 5, 8], [5, 8, 1]]),
+    (2, [[3, 6], [6, 5], [5, 8], [8, 1]]),
+    (1, [[3], [6], [5], [8], [1]]),
+])
+def test_rolling_apply(array, window_size, expected):
+    r = Apply(array, window_size, operation=list)
+    assert list(r) == expected
+
+@pytest.mark.parametrize('array', [[3, 6, 5, 8, 1]])
+@pytest.mark.parametrize('window_size,expected', [
+    (6, [[3], [3, 6], [3, 6, 5], [3, 6, 5, 8], [3, 6, 5, 8, 1]]),
+    (5, [[3], [3, 6], [3, 6, 5], [3, 6, 5, 8], [3, 6, 5, 8, 1],
+         [6, 5, 8, 1], [5, 8, 1], [8, 1], [1]]),
+    (4, [[3], [3, 6], [3, 6, 5], [3, 6, 5, 8], [6, 5, 8, 1],
+         [5, 8, 1], [8, 1], [1]]),
+    (3, [[3], [3, 6], [3, 6, 5], [6, 5, 8], [5, 8, 1], [8, 1], [1]]),
+    (2, [[3], [3, 6], [6, 5], [5, 8], [8, 1], [1]]),
+    (1, [[3], [6], [5], [8], [1]]),
+])
+def test_rolling_apply_variable(array, window_size, expected):
+    r = Apply(array, window_size, operation=list, window_type='variable')
+    assert list(r) == expected
