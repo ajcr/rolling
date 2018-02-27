1f92ae9f2af745508d4561c04948f9123d3cf353
ajcr <ajcriley@gmail.com>
2018-02-27 22:04:50 +0000

    version number - add patch

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ applied to the window. Both fixed-length and variable-length windows are support
 
 setup(
     name='rolling',
-    version='0.1',
+    version='0.1.1',
     description='Efficient rolling window algorithms',
     long_description=long_description,
     classifiers=[
