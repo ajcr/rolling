bb61061ed7e6f71a74454f3055c8eab4bb99f646
Alex Riley <ajcriley@gmail.com>
2021-03-27 16:10:21 +0000

    Bump version number to 0.3.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ applied to the window. Both fixed-length and variable-length windows are support
 
 setup(
     name='rolling',
-    version='0.2.0',
+    version='0.3.0',
     description='Efficient rolling window algorithms',
     long_description=long_description,
     classifiers=[
