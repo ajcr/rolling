1114568f3d03dcf6262484e59b2995698036d1c7
Alex Riley <ajcriley@gmail.com>
2021-11-16 20:23:13 +0000

    Do not override __new__ to set dunder methods in baseclass

--- rolling/base.py
+++ rolling/base.py
@@ -6,13 +6,9 @@ class RollingObject(metaclass=abc.ABCMeta):
     """
     Baseclass for rolling iterator objects.
 
-    The __new__ method here sets appropriate magic
-    methods for the class (__iter__ and __init__)
-    depending on window_type.
-
     All iteration logic is handled in this class.
-    Subclasses just implement methods manipulating
-    any attributes needed to compute the value of
+    Subclasses implement methods manipulating
+    the attributes needed to compute the value of
     the rolling window as values are added and removed.
 
     Subclasses *must* implement the following methods
@@ -23,36 +19,26 @@ class RollingObject(metaclass=abc.ABCMeta):
       _update_window(self, new)
       _add_new(self, new)
       _remove_old(self)
-      current_value(self) # this is a @property
+      current_value(self)
 
     Variable-length instances must also have a self._obs
-    attribute returning the current size of the window.
+    attribute that returns the current size of the window.
 
     """
-
-    def __new__(cls, iterable, window_size, window_type="fixed", **kwargs):
-
-        if window_type == "fixed":
-            cls.__init__ = cls._init_fixed
-            cls.__next__ = cls._next_fixed
-
-        elif window_type == "variable":
-            cls.__init__ = cls._init_variable
-            cls.__next__ = cls._next_variable
-
-        else:
-            raise ValueError("Unknown window_type '{}'".format(window_type))
-
-        self = super().__new__(cls)
-
+    def __init__(self, iterable, window_size, window_type="fixed", **kwargs):
         self.window_type = window_type
         self.window_size = _validate_window_size(window_size)
         self._iterator = iter(iterable)
+        self._filled = self.window_type == "fixed"
 
-        if self.window_type == "variable":
-            self._filled = False
+        if window_type == "fixed":
+            self._init_fixed(iterable, window_size, **kwargs)
 
-        return self
+        elif window_type == "variable":
+            self._init_variable(iterable, window_size, **kwargs)
+
+        else:
+            raise ValueError("Unknown window_type '{}'".format(window_type))
 
     def __repr__(self):
         return "Rolling(operation='{}', window_size={}, window_type='{}')".format(
@@ -62,39 +48,37 @@ class RollingObject(metaclass=abc.ABCMeta):
     def __iter__(self):
         return self
 
-    def _next_fixed(self):
-        """
-        Return the next value for fixed-length windows
-        """
-        new = next(self._iterator)
-        self._update_window(new)
-        return self.current_value
+    def __next__(self):
 
-    def _next_variable(self):
-        """
-        Return the next value for variable-length windows
-        """
-        # while the window size is not reached, add new values
-        if not self._filled and self._obs < self.window_size:
-            new = next(self._iterator)
-            self._add_new(new)
-            if self._obs == self.window_size:
-                self._filled = True
-            return self.current_value
-
-        # once the window size is reached, update window until the iterator finishes
-        try:
+        if self.window_type == "fixed":
             new = next(self._iterator)
             self._update_window(new)
             return self.current_value
 
-        # if the iterator finishes, remove the oldest values one at a time
-        except StopIteration:
-            if self._obs == 1:
-                raise
-            else:
-                self._remove_old()
+        elif self.window_type == "variable":
+            # while the window size is not reached, add new values
+            if not self._filled and self._obs < self.window_size:
+                new = next(self._iterator)
+                self._add_new(new)
+                if self._obs == self.window_size:
+                    self._filled = True
                 return self.current_value
+
+            # once the window size is reached, update until the iterator finishes
+            try:
+                new = next(self._iterator)
+                self._update_window(new)
+                return self.current_value
+
+            # if the iterator finishes, remove the oldest values one at a time
+            except StopIteration:
+                if self._obs == 1:
+                    raise
+                else:
+                    self._remove_old()
+                    return self.current_value
+
+        raise NotImplementedError(f"next() not implemented for {self.window_type}")
 
     def extend(self, iterable):
         """
