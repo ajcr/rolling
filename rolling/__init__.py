74630e65a3cd93f9f11d5b968a97364065c0f843
Alex Riley <ajcriley@gmail.com>
2023-03-11 14:30:28 +0000

    Adjust module imports

--- rolling/__init__.py
+++ rolling/__init__.py
@@ -1,13 +1,12 @@
-from .apply import Apply
-from .entropy import Entropy
-from .hash import PolynomialHash
-from .logical import All, Any
-from .matching import Match
-from .monotonic import Monotonic
-from .minmax import Min, Max, MinHeap
-from .similarity import JaccardIndex
-from .stats import Mean, Var, Std, Median, Mode, Skew, Kurtosis
-
-from rolling.arithmetic import *
+from rolling.apply import Apply
+from rolling.arithmetic import Nunique, Product, Sum
+from rolling.entropy import Entropy
+from rolling.hash import PolynomialHash
+from rolling.logical import All, Any
+from rolling.matching import Match
+from rolling.minmax import Min, Max, MinHeap
+from rolling.monotonic import Monotonic
+from rolling.similarity import JaccardIndex
+from rolling.stats import Mean, Var, Std, Median, Mode, Skew, Kurtosis
 
 __version__ = "0.4.0"
